437ae9194d99a7b2c4da00ce9afd60667b3f94cf
snigdha-pal <85812352+snigdha-pal@users.noreply.github.com>
2021-11-03 19:57:28 +0530

    Update importToSolr_v1.py

--- solr_test/importToSolr_v1.py
+++ solr_test/importToSolr_v1.py
@@ -2,14 +2,8 @@ import json
 import requests
 
 Path_jsonFile = 'employee.json'
-jsonDict = []
-
-with open(Path_jsonFile,'r', encoding='utf-8') as jsonFile:
-        jsonDict = json.load(jsonFile)
-
 url = "http://172.31.6.101:8983/solr/employees/update?commit=true"
 header = {"Content-type":"application/json"}
-payload = jsonDict
 res = requests.post(url, data=open("employee.json", "rb").read(), headers=header)
 pastebin_url = res.text
 print(pastebin_url)
